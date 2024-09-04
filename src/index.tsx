dc864be06b1a5909093cfdd7f4e12394a2fb3a1a
sbleru <usareru@gmail.com>
2024-09-04 14:18:29 +0900

    feat: Implement hyphenation

--- src/index.tsx
+++ src/index.tsx
@@ -1,31 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+// FIXME: これだとうまくいかない
 // import Hyphenopoly from "hyphenopoly";
 
 const rootElement = document.getElementById("root")!;
 const root = ReactDOM.createRoot(rootElement);
 
 // Hyphenopolyの設定
-// if ((window as any).Hyphenopoly) {
-//   (window as any).Hyphenopoly.config({
-//     require: {
-//       "en-us": "FORCEHYPHENOPOLY"
-//     },
-//     paths: {
-//       maindir: "/hyphenopoly/",
-//       patterndir: "/hyphenopoly/patterns/"
-//     },
-//     setup: {
-//       selectors: {
-//         ".hyphenate": {
-//           hyphen: "•"
-//         }
-//       }
-//     }
-//   });
-// }
-
+if ((window as any).Hyphenopoly) {
+  (window as any).Hyphenopoly.config({
+    require: {
+      "en-us": "FORCEHYPHENOPOLY",
+      "de": "FORCEHYPHENOPOLY"
+    },
+    paths: {
+      patterndir: "./hyphenopoly/patterns/"
+    },
+    setup: {
+      selectors: {
+        ".hyphenate": {
+          // ソフトハイフンを使用
+          hyphen: "\u00AD"
+        }
+      }
+    },
+  });
+}
 
 root.render(
   <React.StrictMode>
